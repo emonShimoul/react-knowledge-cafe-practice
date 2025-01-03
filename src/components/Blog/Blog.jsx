8114ee4b518752c03f33136edcda1da406b3a4a3
emonShimoul <emonshimoul@gmail.com>
2025-01-03 17:48:57 +0600

    display blog data and hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,41 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-  console.log(blog);
+  const {
+    title,
+    cover,
+    reading_time,
+    author,
+    author_img,
+    posted_date,
+    hashtags,
+  } = blog;
 
-  return <div></div>;
+  return (
+    <div>
+      <img src={cover} alt={`Cover pic of ${title}`} />
+      <div className="flex justify-between">
+        <div className="flex">
+          <img className="w-12 h-12 rounded-full" src={author_img} alt="" />
+          <div className="ml-6">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="">{hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,18 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState();
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     fetch("blogs.json")
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
+  //   console.log(blogs);
 
   return (
     <div className="md:w-2/3">
-      <h2 className="text-4xl">Blogs: {blogs.length}</h2>
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
