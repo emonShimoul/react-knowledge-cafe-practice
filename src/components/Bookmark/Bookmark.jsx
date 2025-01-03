a7bbabf735bddec47c8a8adc60aca3e500582eda
emonShimoul <emonshimoul@gmail.com>
2025-01-03 19:16:47 +0600

    added design to the bookmarks option

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 const Bookmark = ({ bookmark }) => {
   const { title } = bookmark;
   return (
-    <div>
-      <h2 className="text-3xl">{title}</h2>
+    <div className="bg-slate-200 p-4 m-4 rounded-xl">
+      <h2 className="text-xl">{title}</h2>
     </div>
   );
 };
